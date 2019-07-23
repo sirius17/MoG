5ed1cdda6f1a305afb7088a7deaaf92fa2571698
Nikhil Prasad <nikhil.prasad@gmail.com>
2019-07-23 15:43:40 +0530

    Added conversion logic for number system.

--- MoG.Tests/NumberSystemFixture.cs
+++ MoG.Tests/NumberSystemFixture.cs
@@ -13,5 +13,42 @@ namespace MoG.Tests
             numberSystem.GetAlias("glob").Should().Be(RomanDigit.I);
         }
 
+        [Theory]
+        [InlineData("I", 1)]
+        [InlineData("V", 5)]
+        [InlineData("X", 10)]
+        [InlineData("L", 50)]
+        [InlineData("M", 1000)]
+        [InlineData("D", 500)]
+        [InlineData("C", 100)]
+        [InlineData("I I I", 3)]
+        [InlineData("I V", 4)]
+        [InlineData("V I", 6)]
+        public void Number_system_should_be_able_to_convert_galactic_numbers_to_decimal_test(string number, decimal expected)
+        {
+            var numberSystem = new GalacticNumberSystem();
+            numberSystem.SetAlias(RomanDigit.I, "I");
+            numberSystem.SetAlias(RomanDigit.V, "V");
+            numberSystem.SetAlias(RomanDigit.X, "X");
+            numberSystem.SetAlias(RomanDigit.L, "L");
+            numberSystem.SetAlias(RomanDigit.M, "M");
+            numberSystem.SetAlias(RomanDigit.D, "D");
+            numberSystem.SetAlias(RomanDigit.C, "C");
+            numberSystem.GetDecimalValue(number).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("pish pish", 2)]
+        [InlineData("pish glob", 4)]
+        [InlineData("pish tegj", 9)]
+        public void Alternate_number_system_should_be_able_to_convert_galactic_numbers_to_decimal_test(string number, decimal expected)
+        {
+            var numberSystem = new GalacticNumberSystem();
+            numberSystem.SetAlias(RomanDigit.I, "pish");
+            numberSystem.SetAlias(RomanDigit.V, "glob");
+            numberSystem.SetAlias(RomanDigit.X, "tegj");
+            numberSystem.GetDecimalValue(number).Should().Be(expected);
+        }
     }
+
 }
--- MoG/GalacticNumberSystem.cs
+++ MoG/GalacticNumberSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MoG
 {
@@ -16,6 +17,36 @@ namespace MoG
         public RomanDigit GetAlias(string alias)
         {
             return _aliases[alias];
+        }
+
+        public decimal GetDecimalValue(string number)
+        {
+            var digits = number.Split(' ')
+                               .Select(GetAlias)
+                               .ToArray();
+            if (digits.Length == 1)
+                return (int)digits.Single();
+
+            var pos = 0;
+            var sum = 0;
+            while (pos < digits.Length)
+            {
+                int value = 0;
+                var current = (int)digits[pos];
+                var next = pos < digits.Length - 1 ? (int)digits[pos + 1] : 0;
+                if (next > current)
+                {
+                    value = next - current;
+                    pos += 2;
+                }
+                else
+                {
+                    value = current;
+                    pos++;
+                }
+                sum += value;
+            }
+            return sum;
         }
     }
 
